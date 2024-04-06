9600708f581d44af8d16ed1c510208006507422e
fajarkusumaa <fajarkusuma46@gmail.com>
2024-04-07 01:44:47 +0700

    add icon marker

--- src/components/DynamicPin.js
+++ src/components/DynamicPin.js
@@ -1,14 +1,17 @@
 import { Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
+const customIcon = L.icon({
+  iconUrl: "pin.png",
+  iconSize: [32, 46], // size of the icon
+  iconAnchor: [16, 32], // point of the icon which will correspond to marker's location
+});
+
 // @refresh reset
 
 const DynamicPin = ({ item }) => {
-  const position = [51.505, -0.09];
-  console.log(item);
-
   return (
-    <Marker position={[item._geoloc.lat, item._geoloc.lng]}>
+    <Marker position={[item._geoloc.lat, item._geoloc.lng]} icon={customIcon}>
       <Popup>
         A pretty CSS3 popup. <br /> Easily customizable.
       </Popup>
